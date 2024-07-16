1ae44e6c2d7b00df4ce4b745196a10fd1f97c260
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-07-16 23:11:10 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1146,30 +1146,23 @@ def dropbox(url):
 # shareus
 
 
-def shareus(url):
-    headers = {
-        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
-    }
-    DOMAIN = "https://us-central1-my-apps-server.cloudfunctions.net"
-    sess = requests.session()
-
-    code = url.split("/")[-1]
-    params = {
-        "shortid": code,
-        "initial": "true",
-        "referrer": "https://shareus.io/",
-    }
-    response = requests.get(f"{DOMAIN}/v", params=params, headers=headers)
-
-    for i in range(1, 4):
-        json_data = {
-            "current_page": i,
-        }
-        response = sess.post(f"{DOMAIN}/v", headers=headers, json=json_data)
-
-    response = sess.get(f"{DOMAIN}/get_link", headers=headers).json()
-    return response["link_info"]["destination"]
-
+async def shareus(url):
+    code = url.split('/')[-1]
+    DOMAIN = "https://api.shrslink.xyz"
+    headers = {'User-Agent':'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36', 'Origin':'https://shareus.io'}
+    api = f"{DOMAIN}/v?shortid={code}&initial=true&referrer="
+    async with ClientSession() as session:
+        async with session.get(api, headers=headers) as resp:
+            data = await resp.json()
+            id = data.get('sid')
+            if not id:
+                return "ID Error"
+            else:
+                api_2 = f"{DOMAIN}/get_link?sid={id}"
+                async with session.get(api_2, headers=headers) as resp_2:
+                    data_2 = await resp_2.json()
+                    final = data_2['link_info']['destination']
+                    return final
 
 #######################################################
 # shortingly
